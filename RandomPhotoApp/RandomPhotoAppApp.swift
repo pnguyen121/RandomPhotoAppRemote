460d52219f76c60f06c29fc59f6c52a3e2486b8e
pnguyen121 <phillynguyen17@gmail.com>
2023-06-02 10:49:35 -0400

    Initial Commit

---
+++ RandomPhotoApp/RandomPhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoAppApp.swift
+//  RandomPhotoApp
+//
+//  Created by Phil Nguyen on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
